8df16f2137b6d94ea484c754dc2194553d8108d1
HiHoi <ghtjd5087@gmail.com>
2020-03-05 23:41:24 +0900

    Detect web-page width and change shape

--- bg.js
+++ bg.js
@@ -1,21 +1,52 @@
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 14;
+const IMG_NUMBER_RESIZE = 5;
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `img/${imgNumber + 1}.jpg`;
-    image.classList.add("bgimage");
-    body.prepend(image);
+function resizeBrowser(imgNumber) {
+  const image = new Image();
+  image.src = `img/650/${imgNumber + 1}.jpg`;
+  image.classList.add("bgimage");
+  body.prepend(image);
 }
 
-function getRandom(){
-    const number = Math.floor(Math.random()*IMG_NUMBER);
-    return number;
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `img/basic/${imgNumber + 1}.jpg`;
+  image.classList.add("bgimage");
+  body.prepend(image);
 }
 
-function init(){
-    const randomNumber = getRandom();
+function getRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function getRandom_resize() {
+  const number = Math.floor(Math.random() * IMG_NUMBER_RESIZE);
+  return number;
+}
+
+function refresh(){
+    window.onresize = function(){
+        document.location.reload()
+    }
+}
+
+function resize() {
+  let windowWidth = window.innerWidth;
+  const randomNumber = getRandom();
+  const randomNumber_resize = getRandom_resize();
+  if (windowWidth < 651) {
+    resizeBrowser(randomNumber_resize);
+    refresh()
+  } else {
     paintImage(randomNumber);
+    refresh()
+  }
+}
+
+function init() {
+  resize();
 }
 init();
